1d750eb4bd825a7a2bfa192dc8bbc13c0f0d003d
iovxw <iovxw@outlook.com>
2017-06-03 21:29:56 +0800

    Bindgen

---
+++ build.rs
@@ -0,0 +1,51 @@
+extern crate bindgen;
+
+use std::env;
+use std::path::PathBuf;
+use std::process::Command;
+use std::io::{Error, ErrorKind};
+
+const PSENSOR_DIR: &'static str = "psensor-1.2.0";
+const LIBPSENSOR_DIR: &'static str = "psensor-1.2.0/src/lib";
+
+fn main() {
+    Command::new("./configure")
+        .current_dir(PSENSOR_DIR)
+        .env("CONFIG_FILES", "src/lib/Makefile")
+        .output()
+        .and_then(|output| if output.status.success() {
+                      Ok(())
+                  } else {
+                      let err = String::from_utf8_lossy(&output.stderr);
+                      Err(Error::new(ErrorKind::Other, err.to_string()))
+                  })
+        .expect("failed to configure psensor");
+
+    Command::new("make")
+        .current_dir(LIBPSENSOR_DIR)
+        .output()
+        .and_then(|output| if output.status.success() {
+                      Ok(())
+                  } else {
+                      let err = String::from_utf8_lossy(&output.stderr);
+                      Err(Error::new(ErrorKind::Other, err.to_string()))
+                  })
+        .expect("failed to make libpsensor");
+
+    println!("cargo:rustc-link-search={}", LIBPSENSOR_DIR);
+    println!("cargo:rustc-link-lib=static=psensor");
+
+    let bindings = bindgen::Builder::default()
+        .no_unstable_rust()
+        .clang_arg("-fPIC")
+        .clang_arg("-Ipsensor-1.2.0")
+        .clang_arg("-Ipsensor-1.2.0/src/lib")
+        .header("wrapper.h")
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
---
+++ wrapper.h
@@ -0,0 +1,1 @@
+#include "psensor-1.2.0/src/lib/psensor.h"
