107f4ba7af372c23e2f51957ae2c2eb99071a1dc
iovxw <iovxw@outlook.com>
2017-06-05 23:28:12 +0800

    Update headers

--- wrapper.h
+++ wrapper.h
@@ -1,1 +1,11 @@
-#include "psensor-1.2.0/src/lib/psensor.h"
+#include <psensor.h>
+
+#include <amd.h>
+#include <nvidia.h>
+#include <hdd.h>
+#include <pudisks2.h>
+#include <lmsensor.h>
+
+// #include <pgtop2.h>
+
+// FIXME: 重新声明所有 inline 函数
